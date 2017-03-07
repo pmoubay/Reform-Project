dc8b94a68ab6f41b86e07bae09d635407722fe57
Suchitra S <suchitra.shrinivasans@gmail.com>
2017-03-06 20:19:16 -0500

    updated contact us with feedback form
    without footer.

---
+++ Beta/ContactUs.js
@@ -0,0 +1,7 @@
+function initialize() {
+var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+google.maps.event.addDomListener(window, "resize", function() {
+   var center = map.getCenter();
+   google.maps.event.trigger(map, "resize");
+   map.setCenter(center);
+});
