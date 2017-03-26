159f42f123c23385c94f3880db160aa9705155d3
Suchitra S <suchitra.shrinivasans@gmail.com>
2017-03-26 19:35:03 -0400

    Up to date About Us, Nutrition, Life Coaching
    Updated About Us, Life Coaching and Nutrition Web pages.
    Added a All-in-One css file works fine.
    Added a All-in-OneScript javascript file.

---
+++ Beta/All-in-OneScript.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+
+    $(".mainmenu").addClass("js").before('<div id="menu">&#9776;</div>');
+    $("#menu").click(function() {
+        $(".mainmenu").toggle();
+    });
+
+    $(window).resize(function() {
+        if (window.innerWidth > 880) {
+            $(".mainmenu").removeAttr("style");
+            $(".submenu").removeAttr("style");
+        }
+
+        if (window.innerWidth < 880) {
+            $(".aboutusmenubutton").click(function() {
+                $("#aboutusmenu").toggle();
+            });
+            $(".servicesmenubutton").click(function() {
+                $("#servicesmenu").toggle();
+            });
+            $(".programsmenubutton").click(function() {
+                $("#programsmenu").toggle();
+            });
+        }
+    });
+
+});
+
+/**** ABOUT US JS ****/
+$(document).ready(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top - 100
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+/****  ****/
